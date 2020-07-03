d961342a72f0bb3ae22fd950155d5d122e7192ec
yaxing.chen <chenyaxing@rt-thread.com>
2020-07-03 09:47:42 +0800

    update jlink to 6.80d

--- Samples/RDI/Projects/IAR/ATMEL/AT91SAM7S64/CleanUp.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-FOR %%i IN (Exe, Output, Release, Debug, Settings) DO IF EXIST %%i RD %%i /S/Q
-
-FOR %%i IN (EX~, DEP, OPT, PLG, APS, NCB, TMP, ZIP, LOG, ILK, SIO, ERR, TPU, RES) DO IF EXIST *.%%i DEL *.%%i
